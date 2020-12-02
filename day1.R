e766f16cd55cd1913b0baf8eb1f21f8900795e9c
kiangreene <56272993+kiangreene@users.noreply.github.com>
2020-12-01 19:08:43 -0700

    Update day1.R

--- day1.R
+++ day1.R
@@ -10,3 +10,21 @@ for (i in input){
     }
   }
 }
+#793524
+  
+ #-------------------------------------
+my_wrapper <- function(input){
+  for (i in input){
+    for (j in input) {
+      for (k in input){
+        if (i + j + k == 2020) {
+          return(i * j * k)
+        }
+      }
+    }
+  }
+}
+my_wrapper(input)
+#61515678
+
+
